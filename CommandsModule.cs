c455d448d4f68d1e203344fe646aed8a27942b17
Solomon Oduyebo <124704556+SolomonGM@users.noreply.github.com>
2024-01-18 05:49:42 +0000

    Add files via upload

---
+++ CommandsModule.cs
@@ -0,0 +1,20 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeluBot
+{
+    public class CommandsModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("Shout")]
+        public async Task ShoutCommandAsync()
+        {
+            await ReplyAsync("I am Delulu, your personal Delusional Bot");
+        }
+
+
+    }
+}
---
+++ Hashing.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DeluBot
+{
+    public static class Hashing
+    {
+        public static string ToSHA256(string token)
+        {
+            using var sha256 = SHA256.Create();
+            byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(token));
+
+            var sb = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                sb.Append(bytes[i].ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
